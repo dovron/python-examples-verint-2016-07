c8719294afdf423dd3f0a2741d3e1d67311bffe1
dovron <dovron@gmail.com>
2016-07-12 11:59:58 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,7 +1,18 @@
 """
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name
-Program should write the lines from the first two 
-files interwinded into the output file
+      _      _      _
+   __(.)< __(.)> __(.)=
+   \___)  \___)  \___)   
+
+Python course - Verint
+Week 2
+
 """
 
+import sys
+import binascii
+from itertools import izip_longest
+
+with open(sys.argv[1], "r") as fin1:
+    with open(sys.argv[2], "r") as fin2:
+        with open(sys.argv[3], "w") as fout:
+            print >> fout, list(izip_longest(fin1,fin2,fillvalue=None))
