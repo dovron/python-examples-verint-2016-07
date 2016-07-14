9b302359189fd2e6ff63acc322ff011677b74e66
dovron <dovron@gmail.com>
2016-07-14 15:59:06 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,28 @@
 """
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
+      _      _      _
+   __(.)< __(.)> __(.)=
+   \___)  \___)  \___)   
+
+Python course - Verint
+Week 2
+
 """
+
+import sys
+home = {}
+
+with open('hosts.txt', 'r') as f:
+    for line in f:
+        fields = line.split("=")
+        if len(fields) != 2 : continue
+        (host,ip) = fields
+        ip = ip.strip('\n')
+        home[host] = ip
+
+i = len(sys.argv)
+
+for i in range(1,i):
+    if sys.argv[i] in home:
+        print home[sys.argv[i]]
+    else:
+        print "Error, host:" ,sys.argv[i] ,"not exist"
