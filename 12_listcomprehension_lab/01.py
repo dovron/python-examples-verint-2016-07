1d0f3c95b4887560a57ffe228e23fa135a016b1b
dovron <dovron@gmail.com>
2016-07-17 17:21:56 +0300

    Solution for: sol-python-verint.list-comprehension-lab/01.py

--- 12_listcomprehension_lab/01.py
+++ 12_listcomprehension_lab/01.py
@@ -1,8 +1,13 @@
 """
-Use range() and list comprehension to get
-the list of all lowercase english letters
-Hint: look for chr() and ord()
+      _      _      _
+   __(.)< __(.)> __(.)=
+   \___)  \___)  \___)   
+
+Python course - Verint
+Week 2
+
 """
 
+word = [chr(i) for i in range(97,123)]
 
-
+print word
