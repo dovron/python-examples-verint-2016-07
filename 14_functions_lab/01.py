faa3149dac922cbd472fc7e1fb83260c15f5e364
dovron <dovron@gmail.com>
2016-07-17 22:16:43 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,27 @@
 """
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
+      _      _      _
+   __(.)< __(.)> __(.)=
+   \___)  \___)  \___)   
+
+Python course - Verint
+Week 2
+
 """
 
+def sum_nums(*arg):
+    res = 0
+    for n in arg:
+        if type(n) == int:
+            res += n
+    return res
+
+def mul_nums(*arg):
+    res = 1
+    for n in arg:
+        if type(n) == int:
+            res *= n
+    return res
+
+
+print sum_nums(941,'a',100,100,1001,100)
+print mul_nums('foo', 'bar', 10, 20)
