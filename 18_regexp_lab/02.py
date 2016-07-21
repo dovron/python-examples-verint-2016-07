cfa6d6a5e3156dd66e3be9d5a0246b48471fa886
dovron <dovron@gmail.com>
2016-07-21 18:13:08 +0300

    Solution for: sol-python-verint.regexp-lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -1,6 +1,28 @@
 """
-Write a function toCamelCase(text) that returns the text camel cased
-Write a function to_underscore(text) that returns the text with 
-underscores between words
+      _      _      _
+   __(.)< __(.)> __(.)=
+   \___)  \___)  \___)   
+
+Python course - Verint
+Week 3
+
 """
 
+import re
+
+
+def toCamelCase(word):
+    if re.search(r'(\w+)_(\w+)', word):
+        word = re.sub(r'_([a-z])', lambda m: m.group(1).upper(), word)
+        word = re.sub('_', '', word)
+    return word
+
+    
+def fromCamelCase(word):
+    if re.search(r'[a-z][A-Z]', word):
+        word = re.sub(r'([A-Z])', lambda m:'_' + m.group(0).lower() , word)
+    return word
+
+
+print toCamelCase('no_more_shall_we_part')
+print fromCamelCase('noMoreShallWePart')
